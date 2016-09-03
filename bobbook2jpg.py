3ad79ff81c78943bcc185193fe7b54e02c3cc26c
Chris Baume <chris.baume@bbc.co.uk>
2016-09-03 20:19:01 +0100

    Removed unnecessary for loop

--- bobbook2jpg.py
+++ bobbook2jpg.py
@@ -13,30 +13,34 @@ colorMap = {
 soup = BeautifulSoup(open(filename), 'xml')
 for page in soup.find_all('page'):
     if page['type'] not in ['normalpage','emptypage']: continue
+
     pageNumber=int(page['pagenr'])
-    print('Processing page {}...'.format(pageNumber))
     pageHeight = int(page.bundlesize['height'])
     pageWidth = int(page.bundlesize['width'])
     pageColor = colorMap[page.background['templatename']]
     pageImg = Image.new('RGBA', (pageWidth, pageHeight), pageColor)
     pageIsEmpty=True
+    print('Processing page {}...'.format(pageNumber))
+
     for area in page.find_all('area'):
+        pageIsEmpty=False
         left = float(area['left'])
         top = float(area['top'])
         width = float(area['width'])
         height = float(area['height'])
-        for image in area.find_all('image'):
-            print('  Adding '+image['filename'])
-            img = Image.open(imgFolder+'/'+image['filename'])
-            scale = float(image['scale'])
-            imgLeft = float(image['left'])*-1
-            imgTop = float(image['top'])*-1
-            imgWidth = width/scale
-            imgHeight = height/scale
-            img = img.crop((imgLeft, imgTop, imgLeft+imgWidth, imgTop+imgHeight))
-            img = img.resize((int(width), int(height)))
-            pageImg.paste(img, (int(left), int(top), int(left)+int(width), int(top)+int(height)))
-            pageIsEmpty=False
+
+        print('  Adding '+area.image['filename'])
+        img = Image.open(imgFolder+'/'+area.image['filename'])
+        scale = float(area.image['scale'])
+        imgLeft = float(area.image['left'])*-1
+        imgTop = float(area.image['top'])*-1
+        imgWidth = width/scale
+        imgHeight = height/scale
+
+        img = img.crop((imgLeft, imgTop, imgLeft+imgWidth, imgTop+imgHeight))
+        img = img.resize((int(width), int(height)))
+        pageImg.paste(img, (int(left), int(top), int(left)+int(width), int(top)+int(height)))
+
     if pageIsEmpty:
         print('No images in page {}. Skipping...'.format(pageNumber))
     else:
